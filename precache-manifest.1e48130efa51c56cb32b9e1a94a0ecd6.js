943ac11a87ebb2338ee364ca8b967bc9d92dd919
Matumba125 <nbenza5@gmail.com>
2021-07-06 21:49:13 +0300

    Updates

---
+++ precache-manifest.1e48130efa51c56cb32b9e1a94a0ecd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc6dcc992c1ca50ef3c66c16701f5da",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b1b6b9bfbbf84f22e0c5",
+    "url": "/todolist/static/css/main.77d0495b.chunk.css"
+  },
+  {
+    "revision": "3d581c6cf992a61fa3ae",
+    "url": "/todolist/static/js/2.d5e2b886.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.d5e2b886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b6b9bfbbf84f22e0c5",
+    "url": "/todolist/static/js/main.008e42c7.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  },
+  {
+    "revision": "1fb941fbf19cee354a3865569a449ade",
+    "url": "/todolist/static/media/Seoul.1fb941fb.jpg"
+  }
+]);
